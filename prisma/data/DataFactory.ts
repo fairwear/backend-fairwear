67d847291f35cc537ba2f5429bb5467feb3c4184
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-03-07 11:19:16 +0200

    FAIR-25 docker-compose.local.yml + prisma seed & datafactory + package.json update & gitignore update + dotenv config in main.ts

---
+++ prisma/data/DataFactory.ts
@@ -0,0 +1,12 @@
+export class DataFactory {
+  public static instance: DataFactory;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public constructor() {}
+
+  public static getInstance(): DataFactory {
+    if (!DataFactory.instance) {
+      DataFactory.instance = new DataFactory();
+    }
+    return DataFactory.instance;
+  }
+}
