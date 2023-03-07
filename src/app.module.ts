1e56480cacd650e5a16d5d381127ef30af324350
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-03-07 17:58:04 +0200

    FAIR-8 email draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot(), EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
