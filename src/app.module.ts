69a187c2bf5024e0b643cae0bb1c3889c9bf901d
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2023-03-10 21:53:39 +0200

    merge simpson

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { EmailtemplateModule } from './emailtemplate/emailtemplate.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserRoleModule } from './user-role/user-role.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [EmailtemplateModule, PrismaModule, ConfigModule.forRoot()],
+  imports: [
+    EmailtemplateModule,
+    PrismaModule,
+    ConfigModule.forRoot(),
+    UserRoleModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
