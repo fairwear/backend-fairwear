5740d1b7add2ef4c704ac2f17f7ca971fba48166
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-03-15 09:00:43 +0200

    FAIR-26 updated Pirsma Schema & DataFactory

--- prisma/data/DataFactory.ts
+++ prisma/data/DataFactory.ts
@@ -1,4 +1,8 @@
 import { EmailTemplateEntity } from '../../src/emailtemplate/entities/emailtemplate.entity';
+import { InfoPostToTopic } from '../../src/infopost/entities/infopost-to-topic.entity';
+import { InfoPostVote } from '../../src/infopost/entities/infopost-vote.entity';
+import { InfoPostEntity } from '../../src/infopost/entities/infopost.entity';
+import { VoteEnum } from '@prisma/client';
 
 export class DataFactory {
   public static instance: DataFactory;
@@ -53,5 +57,87 @@ export class DataFactory {
     emailTemplateList.push(emailTemplate3);
 
     return emailTemplateList;
+  }
+
+  public getValidInfoPost() {
+    const infoPost: InfoPostEntity = new InfoPostEntity();
+    infoPost.id = 1;
+    // infoPost.author = this.getValidUser();
+    // infoPost.item = this.getValidItem();
+    infoPost.votes = this.getInfoPostVoteList();
+    infoPost.topics = this.getInfoPostToTopicsList();
+    infoPost.createdAt = new Date('2021-01-01T00:00:00.000Z');
+    infoPost.updatedAt = new Date('2021-01-02T00:00:00.000Z');
+    infoPost.isDeleted = false;
+    infoPost.deletedAt = null;
+
+    return infoPost;
+  }
+
+  public getValidInfoPostToTopic() {
+    const infoPostToTopic: InfoPostToTopic = new InfoPostToTopic();
+    infoPostToTopic.infoPostId = 1;
+    infoPostToTopic.topicId = 1;
+    infoPostToTopic.score = 5;
+
+    return infoPostToTopic;
+  }
+
+  public getInfoPostToTopicsList() {
+    const infoPostToTopicsList: InfoPostToTopic[] = [];
+
+    const infoPostToTopic1: InfoPostToTopic = this.getValidInfoPostToTopic();
+
+    const infoPostToTopic2: InfoPostToTopic = new InfoPostToTopic();
+    infoPostToTopic2.infoPostId = 1;
+    infoPostToTopic2.topicId = 2;
+    infoPostToTopic2.score = 10;
+
+    const infoPostToTopic3: InfoPostToTopic = new InfoPostToTopic();
+    infoPostToTopic3.infoPostId = 1;
+    infoPostToTopic3.topicId = 3;
+    infoPostToTopic3.score = 1;
+
+    infoPostToTopicsList.push(infoPostToTopic1);
+    infoPostToTopicsList.push(infoPostToTopic2);
+    infoPostToTopicsList.push(infoPostToTopic3);
+
+    return infoPostToTopicsList;
+  }
+  public getInfoPostVote() {
+    const infoPostVote: InfoPostVote = new InfoPostVote();
+    infoPostVote.id = 1;
+    infoPostVote.infoPostId = 1;
+    infoPostVote.userId = 1;
+    infoPostVote.vote = VoteEnum.UPVOTE;
+    infoPostVote.createdAt = new Date('2021-01-01T00:00:00.000Z');
+
+    return infoPostVote;
+  }
+
+  public getInfoPostVoteList() {
+    const infoPostVoteList: InfoPostVote[] = [];
+
+    const infoPostVote1: InfoPostVote = this.getInfoPostVote();
+
+    const infoPostVote2: InfoPostVote = new InfoPostVote();
+    infoPostVote2.id = 2;
+    infoPostVote2.infoPostId = 1;
+    infoPostVote2.userId = 2;
+    infoPostVote2.vote = VoteEnum.DOWNVOTE;
+    infoPostVote2.createdAt = new Date('2021-01-02T00:00:00.000Z');
+
+    const infoPostVote3: InfoPostVote = new InfoPostVote();
+    infoPostVote3.id = 3;
+    infoPostVote3.infoPostId = 1;
+    infoPostVote3.userId = 3;
+    infoPostVote3.vote = VoteEnum.UPVOTE;
+    infoPostVote3.createdAt = new Date('2021-01-03T00:00:00.000Z');
+
+    infoPostVoteList.push(infoPostVote1);
+    infoPostVoteList.push(infoPostVote2);
+    infoPostVoteList.push(infoPostVote3);
+
+    return infoPostVoteList;
   }
 }
