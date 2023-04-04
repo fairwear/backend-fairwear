868d043d6e7d88dccffddbca14066cdffbaf6a49
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-04-04 12:41:41 +0300

    FAIR-36 auth service test temp fix

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,41 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy } from 'jest-mock-extended';
+import { DataFactory } from '../../prisma/data/DataFactory';
+import { MockContext, createMockContext } from '../prisma/context';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserRoleService } from '../user-role/user-role.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockContext: MockContext;
+  let prismaService: DeepMockProxy<PrismaClient>;
+  const dataFactory: DataFactory = new DataFactory();
 
   beforeEach(async () => {
+    jest.resetAllMocks();
+    mockContext = createMockContext();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      imports: [ConfigModule],
+      providers: [
+        AuthService,
+        UserService,
+        JwtService,
+        UserRoleService,
+        PrismaService,
+      ],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockContext.prisma)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
+    prismaService = module.get<DeepMockProxy<PrismaClient>>(PrismaService);
   });
 
   it('should be defined', () => {
