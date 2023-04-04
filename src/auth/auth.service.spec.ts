9a94ce4ced53059d021d911ab0875be8880c8211
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-04-04 17:43:47 +0300

    FAIR-36 merge fix

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,20 +1,14 @@
 import { ConfigModule } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient } from '@prisma/client';
 import { DeepMockProxy } from 'jest-mock-extended';
 import { DataFactory } from '../../prisma/data/DataFactory';
 import { MockContext, createMockContext } from '../prisma/context';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserRoleService } from '../user-role/user-role.service';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
-import { MockContext, createMockContext } from '../prisma/context';
-import { DeepMockProxy } from 'jest-mock-extended';
-import { PrismaClient } from '@prisma/client';
-import { ConfigModule } from '@nestjs/config';
-import { UserService } from '../user/user.service';
-import { UserRoleService } from '../user-role/user-role.service';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
