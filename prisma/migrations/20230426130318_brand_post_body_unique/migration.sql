5a19814bb0c2a599aef2395251226eb72d95133c
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-06-04 08:20:51 +0300

    FAIR-98 fix reset migrations, merge all to one

--- prisma/migrations/20230426130318_brand_post_body_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[body]` on the table `brand_post` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "brand_post_body_key" ON "brand_post"("body");
